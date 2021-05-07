eb839cd6199e70ac7fcf89ec20740b2b966c2d3c
Archilon <Vehemex@gmail.com>
2021-05-07 15:07:38 +0300

    changes made 07/05/2021 during class

--- Base/Main/Main.java
+++ Base/Main/Main.java
@@ -1,12 +1,23 @@
 package Main;
-import java.util.Random;
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        boolean mainMenu = true;
+        Scanner sc = new Scanner(System.in);
         world r= new world();
-        r.run();
+        while(mainMenu) {
+            System.out.println("\n\n\tinput Anything to continue\n\tOr x to exit game");
+            String mm = sc.nextLine();
+            if(mm.contains("x")){
+                mainMenu=false;
+
+                System.out.println("shutting down");
+            }else{
+                r.run();}
+        }
 
 
-	// write your code here
+        // write your code here
     }
 }
--- Base/Main/player.java
+++ Base/Main/player.java
@@ -1,7 +1,4 @@
 package Main;
-
-import java.util.ArrayList;
-
 //this class contains the player constructor
 public class player {
     private String attackType;
@@ -9,53 +6,86 @@ public class player {
     private String classType;
     private double armor;
     private double HP;
-    private Double maxHP;
+    private double maxHP;
     private double ATK;
+    private int numberHealthPotions = 3;
+    private double healthPotion = 25.0;
+    private int HPPotionDropChance = 25;
+
     //int lvl;
     //double xp;
-
-
-        public String getName() {
+    public String getName() {
         return name;
     }
-        public void setName(String name) {
+    public void setName(String name) {
         this.name = name;
     }
 
-        public String getClassType() {
+    public String getClassType() {
         return classType;
     }
-
-        public void setClassType(String classType) {
+    public void setClassType(String classType) {
         this.classType = classType;
     }
 
-
-        public double getHP() {
+    public double getHP() {
         return HP;
     }
-        public void setHP(double HP) {
+    public void setHP(double HP) {
         this.HP = HP;
     }
 
-        public double getArmor() {
+    public double getArmor() {
         return armor;
     }
-        public void setArmor(double armor) {
+    public void setArmor(double armor) {
         this.armor = armor;
     }
 
-        public double getATK() {
+    public double getATK() {
         return ATK;
     }
-        public void setATK(int ATK) {
+    public void setATK(int ATK) {
         this.ATK = ATK;
     }
 
-        public String getAttackType() {
-        return attackType;
+    public int getNumberHealthPotions(){
+        return numberHealthPotions;
+    }            //getter for number of healthpotions
+    public void useHealthPotion(){
+        if (HP == maxHP) {
+            System.out.println("You are currently at max HP, no potions used.");
+        }else if(numberHealthPotions > 0) {
+            this.HP = HP + healthPotion;
+            numberHealthPotions--;
+
+            System.out.println("\t>You used a health-potion, healing yourself by " + healthPotion + " you currently have: " + numberHealthPotions + " potions left\n");
+            healthCheck();
+        }else{
+            System.out.println("\t>You don't have any health-potions left.\n");
+        }
+    }           //method for using healthpotions
+    public void pickUpHealthPotion(){
+        numberHealthPotions++;}
+    public void healthCheck(){
+        if(HP>maxHP){
+            HP=maxHP;
+            System.out.println("\t>you healed to max HP\n");
+
+        }
+
+
     }
-        public void setAttackType(String attackType) {
+    public int getHPPotionDropChance(){
+        return HPPotionDropChance;
+
+    }
+
+    public String getAttackType() {
+        return attackType;
+
+    }
+    public void setAttackType(String attackType) {
         this.attackType = attackType;
     }
 
